87750fa806b4ad1c547f524cb93a41543ff48f45
deepakp793 <deepak.patil793@gmail.com>
2026-06-16 13:49:46 +0530

    learn structured_output

--- LANGCHAIN-STRUCTURED-OUTPUT/pydantic_demo.py
+++ LANGCHAIN-STRUCTURED-OUTPUT/pydantic_demo.py
@@ -1,8 +1,45 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
 
 class Student(BaseModel):
-    name : str
+    name: str
+    age: int
 
-new_student = Student(name='Deepak')
+
+new_student = Student(name="Deepak", age=31)
 
 print(new_student)
+
+
+class Employee(BaseModel):
+    emp_name: str = "Deepak"
+    emp_id: int
+    emp_age: Optional[int] = None
+    emp_role: str
+    email: EmailStr
+    emp_rating: float = Field(
+        gt=0,
+        lt=10,
+        description="A decimal value representing employees annual performane",
+    )
+
+
+new_employee = {
+    "name": "Deepak",
+    "emp_id": "453",
+    "emp_role": "Gen AI Engineer",
+    "email": "deepak@AIEngineer.com",
+    "emp_rating": "9.5",
+}
+
+employee = Employee(**new_employee)
+
+print(employee)
+
+emp_dict = employee.model_dump()
+print(emp_dict["emp_role"])
+
+emp_json = employee.model_dump_json()
+print(emp_json)
---
+++ LANGCHAIN-STRUCTURED-OUTPUT/with_structured_output_json.py
@@ -0,0 +1,80 @@
+from langchain_openai import ChatOpenAI
+from typing import Optional
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-5.5-2026-04-23")
+
+# schema
+json_schema = {
+    "$defs": {},
+    "title": "Review",
+    "type": "object",
+    "properties": {
+        "key_theme": {
+            "items": {"type": "string"},
+            "type": "array",
+            "title": "Key Theme",
+            "description": "All the key theme of product",
+        },
+        "summary": {
+            "type": "string",
+            "title": "Summary",
+            "description": "A brief summary of review",
+        },
+        "rating": {
+            "type": "integer",
+            "title": "Rating",
+            "description": "Overall rating of product based on review.Rate product between 1 to 10",
+        },
+        "pros": {
+            "anyOf": [{"items": {"type": "string"}, "type": "array"}, {"type": "null"}],
+            "default": None,
+            "title": "Pros",
+            "description": "Write the pros inside list if abailable",
+        },
+        "cons": {
+            "anyOf": [{"items": {"type": "string"}, "type": "array"}, {"type": "null"}],
+            "default": None,
+            "title": "Cons",
+            "description": "Write the cons inside list if abailable",
+        },
+        "name": {
+            "anyOf": [{"type": "string"}, {"type": "null"}],
+            "default": None,
+            "title": "Name",
+            "description": "Set the name only if reveiwer real human name explicitly available.",
+        },
+    },
+    "required": ["key_theme", "summary", "rating"],
+}
+
+structured_model = model.with_structured_output(json_schema)
+
+result = structured_model.invoke(
+    """This is first amkette product purchase and I am fully satisfied with the product . I am using the keyboard for around 10 days
+
+
+Design and Build (4/5) : Good color option and steady plastic build
+
+
+Battery (5/5) : Great battery life . I Recharged the keyboard to 90% just after delivered and still 50%+ battery left after 10 days on average use of 6hrs/day on 2 devices
+
+
+Keys (4/5) : Smooth and responsive keys suitable for every day use
+
+
+Connectivity (3.5/5) : The connectivity is good but experienced Bluetooth connection issue once which is rare and mangeble in every day use.The device switching is seamless as well
+
+
+Weight and Size (4/5) : Lightweight and compact suitable for travelling .
+
+The compact size may be an issue for people who are frequently using full size keyboard initially they can prefer the full size one . The issue is not with the device it's all about the choice
+Review by Andy                                 
+"""
+)
+
+print(result)
+print(result["cons"])
